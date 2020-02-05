f6a677b611213e608c8e3585c46e721cdf666d5e
willmaschmedt <willmaschmedt@seattleacademy.org>
2020-02-05 11:12:40 -0800

    add web page server

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-console.log('Hello World');
+console.log('Hello Universe')
+const express = require('express')
+const app = express()
+const port = 1535
+Let counter = 0
+
+app.use(express.static('public'));
+
+app.get('/hello', (req, res) => res.send('Hello Maschmedt!'))
+
+app.post('/counter', function (req, res) {
+counter = counter + 1
+  res.send('Counter ' + counter)
+})
+
+
+app.post('/counter', function (req, res) {
+counter = counter + 1
+  res.send('Date ' + d)
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
